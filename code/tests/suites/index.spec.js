8bab191576af84acbf901402332884593df233be
jose teodoro da silva <jose.teodoro@dasadigital.com.br>
2020-06-26 21:47:33 -0300

    adding fp content

--- code/tests/suites/index.spec.js
+++ code/tests/suites/index.spec.js
@@ -1,5 +1,29 @@
+const faker = require('faker')
+const R = require('ramda')
+
+const build = size => [...new Array(size)]
+
 describe('Simple test', () => {
-  it('looks how it works', () => {
+  it.skip('looks how it works', () => {
+    const full = build(300)
+      .map(() => faker.helpers.createCard())
+      .map(u => {
+        const id = faker.finance.account()
+        return {
+          ...u,
+          id,
+          account: {
+            id,
+            amout: faker.finance.amount(),
+            iban: faker.finance.iban(),
+          },
+        }
+      })
+      .map(R.omit(['posts']))
+
+    console.log('users: ', JSON.stringify(full.map(R.omit(['company', 'accountHistory', 'account']))))
+    console.log('\n\n\n\n\n')
+    console.log('data: ', JSON.stringify(full.map(R.pick(['id', 'company', 'accountHistory', 'account']))))
     expect(true).to.be.true
   })
 })
